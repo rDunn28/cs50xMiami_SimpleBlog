81f04344ae6476ce532f0a4e94c6ce9a61ce3e95
rDunn28 <r.dunn2474@gmail.com>
2019-06-01 12:36:37 -0400

    Finish the blog tutorial

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -27,9 +27,17 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return 'Hello, World!'
+        return 'Hello, World!, This is Ryan!!'
 
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
 
     return app
